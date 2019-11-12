16348bf20745ec90ecbd9f7ca35bcf46f3ad906d
Swetha Sankar <swetha@udel.edu>
2019-11-12 15:51:07 -0500

    added setup

--- introduction.py
+++ introduction.py
@@ -2,31 +2,29 @@ import arcade
 
 
 # Define constants
-WINDOW_WIDTH = 600
-WINDOW_HEIGHT = 600
-BACKGROUND_COLOR = arcade.color.BLUE
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 500
+BACKGROUND_COLOR = arcade.color.WHITE
 GAME_TITLE = "Hot Air Adventure"
 GAME_SPEED = 1/60
 
 
 class Introduction(arcade.Window):
-    start = arcade.Sprite("images/coin.jpg", .25)
-    intro = arcade.Sprite("images/intro.jpg", 1)
+    start = arcade.Sprite("images/intro_screen.png")
 
     def __init__(self):
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE)
+
+    def setup(self):
+        arcade.set_background_color(BACKGROUND_COLOR)
 
     def start_new_game(self):
         self.on_draw()
 
     def on_draw(self):
         arcade.start_render()
-        self.intro.center_x = WINDOW_WIDTH / 2
-        self.intro.center_y = WINDOW_HEIGHT / 2
-        self.intro.draw()
-
         self.start.center_x = WINDOW_WIDTH / 2
-        self.start.center_y = WINDOW_HEIGHT / 4
+        self.start.center_y = WINDOW_HEIGHT / 2
         self.start.draw()
 
     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
