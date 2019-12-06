0c3c49843f5e1973f067c969e824a6917ea4ed23
Swetha Sankar <swetha@udel.edu>
2019-12-05 19:14:36 -0500

    Added docstrings & final edits.

--- maingame.py
+++ maingame.py
@@ -3,42 +3,53 @@ from sprites import *
 
 class Introduction(arcade.View):
     '''
-
-
-
+    This is a class for the first view that the user sees. It transitions to the How to Play view when the user clicks
+    on the screen.
     '''
     start = arcade.Sprite("images/intro_screen.png")
 
     def on_draw(self):
-        ''' Defines '''
+        ''' Defines position of the introduction image and sets up screen'''
         arcade.start_render()
         self.start.center_x = WINDOW_WIDTH / 2
         self.start.center_y = WINDOW_HEIGHT / 2
         self.start.draw()
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
+        ''' Function that transitions the view to the How to Play screen when the user clicks on the screen '''
         playing = HowToPlay()
         self.window.show_view(playing)
 
 
 class HowToPlay(arcade.View):
+    '''
+    This is a class for the second view that the user sees. It transitions to the main game view (MainGame())
+    when the user clicks anywhere on the screen.
+    '''
     play = arcade.Sprite("images/instructions.png")
 
     def on_draw(self):
+        ''' Function that positions instructions image and sets up screen'''
         arcade.start_render()
         self.play.center_x = WINDOW_WIDTH / 2
         self.play.center_y = WINDOW_HEIGHT / 2
         self.play.draw()
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
-        level_one = LevelOne()
+        '''Function that transitions game view to the first level when the user clicks on the screen'''
+        level_one = MainGame()
         self.window.show_view(level_one)
 
 
 class GameOver(arcade.View):
+    '''
+    This is a class that represents the Game Over view. It appears when the user has less than 25 points after
+    1 minute has elapsed.
+    '''
     game_over = arcade.Sprite("images/game_over.png")
 
     def on_draw(self):
+        '''Function that positions game over image on screen and sets up view.'''
         arcade.start_render()
         self.game_over.center_x = WINDOW_WIDTH / 2
         self.game_over.center_y = WINDOW_HEIGHT / 2
@@ -46,17 +57,29 @@ class GameOver(arcade.View):
 
 
 class GameWon(arcade.View):
+    '''
+    This is a class that represents the screen that appears if a user wins the game. This screen appears
+    if the user is able to obtain 25 points in under a minute for all three levels.
+    '''
     game_won = arcade.Sprite("images/game_won.png")
 
     def on_draw(self):
+        '''This function sets up the game won image and positions it on the screen.'''
         arcade.start_render()
         self.game_won.center_x = WINDOW_WIDTH / 2
         self.game_won.center_y = WINDOW_HEIGHT / 2
         self.game_won.draw()
 
 
-class LevelOne(arcade.View):
+class MainGame(arcade.View):
+    '''
+    This class represents the main game view. It consists of three levels, and transitions to either the GameWon() or
+    GameOver() class depending on the user's score and performance.
+    '''
     def __init__(self):
+        '''
+        This function initializes variables used throughout the course of main game
+        '''
         super().__init__()
         self.center_x = WINDOW_WIDTH / 2
         self.center_y = WINDOW_HEIGHT / 2
@@ -79,9 +102,11 @@ class LevelOne(arcade.View):
         self.player_sprite = Player("images/player.png", scale=.15)
 
     def on_show(self):
+        '''This function calls the level_1 function when the game begins.'''
         self.level_1()
 
     def level_1(self):
+        '''This function sets up the first level. It puts the coins, buildings, and background color onto the screen.'''
         self.total_time = 0.0
         arcade.set_background_color(arcade.color.SKY_BLUE)
         arcade.play_sound(self.level_1_music)
@@ -100,6 +125,10 @@ class LevelOne(arcade.View):
             self.building_list.append(building)
 
     def level_2(self):
+        '''
+        This function sets up the second level of the game.
+        It changes the background color, adds coins, and resets the player sprite's position.
+        '''
         self.total_time = 0.0
         arcade.set_background_color(arcade.color.SUNSET_ORANGE)
         self.player_sprite.center_x = 0
@@ -114,6 +143,9 @@ class LevelOne(arcade.View):
             self.coin_list.append(coin)
 
     def level_3(self):
+        ''' This function sets up the screen for the third level. It resets the player's position, score, and time.
+        It also adds coins and buildings to the screen, and changes the background color.
+        '''
         self.total_time = 0.0
         arcade.set_background_color(arcade.color.BLACK)
         self.player_sprite.center_x = 0
@@ -135,6 +167,7 @@ class LevelOne(arcade.View):
             self.building_list.append(building)
 
     def setup(self):
+        '''This function sets up the game.'''
         self.level = 1
         self.building_list = arcade.SpriteList()
         self.coin_list = arcade.SpriteList()
@@ -142,6 +175,7 @@ class LevelOne(arcade.View):
         self.player_list.append(self.player_sprite)
 
     def on_draw(self):
+        ''' This function places the score, coins, buildings, timer, player, and level onto the screen.'''
         arcade.start_render()
         self.player_sprite.draw()
         self.building_list.draw()
@@ -157,6 +191,12 @@ class LevelOne(arcade.View):
         arcade.draw_text(output, self.player_sprite.center_x, 45, arcade.color.WHITE, 20)
 
     def on_update(self, delta_time):
+        '''
+        This function is called for every frame of the game (1/GAME_SPEED times per second).
+        It updates the score and level according to user performance.
+        It also updates the viewport depending on how far right the user has positioned the sprite.
+        It calls the GameOver() and GameWon() classes depending on the state of the user's score.
+        '''
         self.player_sprite.update()
         self.building_list.update()
         self.coin_list.update()
@@ -200,6 +240,10 @@ class LevelOne(arcade.View):
             self.window.show_view(game_won)
 
     def on_key_press(self, key, modifiers):
+        '''
+        This function is called whenever the user presses an arrow key. It moves the balloon sprite by the
+        movement speed (6).
+        '''
         if key == arcade.key.UP:
             self.player_sprite.change_y = MOVEMENT_SPEED
         elif key == arcade.key.DOWN:
@@ -210,6 +254,10 @@ class LevelOne(arcade.View):
             self.player_sprite.change_x = MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
+        '''
+        This function is called whenever the user releases one of the arrow keys.
+        It ceases movement of the balloon sprite.
+        '''
         if key == arcade.key.UP or key == arcade.key.DOWN:
             self.player_sprite.change_y = 0
         elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
@@ -217,6 +265,7 @@ class LevelOne(arcade.View):
 
 
 def main():
+    ''' This main function sets up the window, and calls the Introduction() class. '''
     window = arcade.Window(WINDOW_WIDTH, WINDOW_HEIGHT, "Hot Air Adventure")
     introduction = Introduction()
     window.show_view(introduction)
--- sprites.py
+++ sprites.py
@@ -3,9 +3,14 @@ from constants import *
 
 class Player(arcade.Sprite):
     '''
-
+    This class represents the player sprite, and defines the bounds of movement for the player.
     '''
     def update(self):
+        '''
+        This function defines the bounds of movement for the player.
+        The player can move off the screen to the right, and the viewport will adjust to their movement.
+        The player cannot move off the screen in any other direction.
+        '''
         self.center_x += self.change_x
         self.center_y += self.change_y
         if self.left < 0:
@@ -19,12 +24,19 @@ class Player(arcade.Sprite):
 
 
 class Building(arcade.Sprite):
+    '''
+    This class represents the building sprite, and moves the buildings upwards on every update.
+    This creates a time-based challenge for the player.
+    '''
     def update(self):
+        '''This function moves the buildings upward by .1 on every update.'''
         self.center_y += .1
 
 
 class Coin(arcade.Sprite):
+    '''This class represents the coin sprite, and creates the falling and rotating coin movement.'''
     def update(self):
+        '''This function creates the falling and rotating coin movement.'''
         self.center_y -= 2
         if self.top < 0:
             self.bottom = WINDOW_HEIGHT
